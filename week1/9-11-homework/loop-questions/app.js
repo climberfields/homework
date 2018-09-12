d836122630a8e765d4590628b12b1520ec54d12a
Alex <climberfields@gmail.com>
2018-09-11 23:47:08 -0500

    did loop questions

---
+++ week1/9-11-homework/loop-questions/app.js
@@ -0,0 +1,8 @@
+1. let i = 0 while (i <= 99) { console.log(i); i++;}
+2. let i = 99 while (i >= 0) {console.log(i); i--;}
+3. let i = 289 while (i > 2) {console.log(i); i-=3;}
+4. 
+5. 
+6. let i = 49 while (i <= 72) { console.log(i); i++;}
+7. let i = 81 while (i >= 26) {console.log(i); i--;}
+8. let i = 1 while (i <= 91) {console.log(i); i*=3;}
