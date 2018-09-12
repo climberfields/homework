57d9ca1cf7afe92b6e51cfddc0350c15866c9b04
Alex <climberfields@gmail.com>
2018-09-12 09:38:52 -0500

    updated the loop statements

--- week1/9-11-homework/loop-questions/app.js
+++ week1/9-11-homework/loop-questions/app.js
@@ -2,7 +2,21 @@
 2. let i = 99 while (i >= 0) {console.log(i); i--;}
 3. let i = 289 while (i > 2) {console.log(i); i-=3;}
 4. 
+for (var i = 0; i <= 98; i++) { 
+  if (i % 2 === 0) {
+    console.log(i)
+  }
+}
+
+
 5. 
+for (var i = 0; i <= 98; i++) { 
+  if (i % 2 === 1) {
+    console.log(i)
+  }
+}
+
+
 6. let i = 49 while (i <= 72) { console.log(i); i++;}
 7. let i = 81 while (i >= 26) {console.log(i); i--;}
 8. let i = 1 while (i <= 91) {console.log(i); i*=3;}
