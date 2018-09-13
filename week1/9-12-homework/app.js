3fa5934e0c0a74c6cc7df981e60473151cbe5407
Alex <climberfields@gmail.com>
2018-09-12 22:42:21 -0500

    forgot to save app.js

--- week1/9-12-homework/app.js
+++ week1/9-12-homework/app.js
@@ -1,5 +1,9 @@
-1. 
-let i = 0
+1. // loop to 20
+let i = 0;
 while (i <= 20) { console.log(i); i++}
 
-2.
+2. // loop to 200 onlyy even integers
+for ( let i = 0; i <= 200; i++) {
+if ( i % 2 === 0) { console.log(i)} }
+
+3.
