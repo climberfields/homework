2ed39a8ab883da38a61d6ceb5db731c5b7845da2
Alex <climberfields@gmail.com>
2018-09-13 00:26:31 -0500

    fizzbuzz and turtles down

--- week1/9-12-homework/app.js
+++ week1/9-12-homework/app.js
@@ -6,4 +6,23 @@ while (i <= 20) { console.log(i); i++}
 for ( let i = 0; i <= 200; i++) {
 if ( i % 2 === 0) { console.log(i)} }
 
-3.
+3. // random saying 
+
+
+4. // fizzbuzz
+for ( let i = 0; i <= 100; i++) {
+ if ( i % 3 === 0) {console.log('fizz');} 
+ else if (i % 5 ===0) console.log('buzz');
+  else if  (i % 3 === 0 && i % 5 === 0)
+   console.log('fizzbuzz'); else {console.log(i);}}
+
+5. // Array of GA folks
+
+
+6. // Ninja Turtles
+
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+
+
+
+for ( let i = 0; i <= ninjaTurtles.length; i++) {console.log(ninjaTurtles[i].toUpperCase());}
