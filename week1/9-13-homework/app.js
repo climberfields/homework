e217bd6409c1c12346c901faf427755b4076a061
Alex <climberfields@gmail.com>
2018-09-13 23:54:58 -0500

    verbal and pythag done

---
+++ week1/9-13-homework/app.js
@@ -0,0 +1,25 @@
+1. // Verbal Questions
+a. A parameter is when you are defining a function. An argument is what you place when you are calling that function. 
+b. When you return a function you are producing the value however it may not print in the console. With console.log you are p rinting to the console the result of the function. 
+c. The function is called and required to be completed. It is implied that the arguments should be completed. 
+
+2. // Palindrome
+
+const checkPalindrome = () => {}
+
+3. // Digit Sum
+
+const sumDigits = (num) => {
+
+}
+
+4. // Pythagoras
+
+const calculateSide = (num1, num2) => {
+	var a = Math.pow(num1, 2);
+	var b = Math.pow(num2, 2);
+	var c = a + b;
+	var d = Math.sqrt(c, 2)
+	console.log(d);
+}
+calculateSide(8, 6);
