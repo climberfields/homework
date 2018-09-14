a84bed3b65e3e5267fdf404934c7369f21b10790
Alex <climberfields@gmail.com>
2018-09-14 09:26:47 -0500

    palindrome done

--- week1/9-13-homework/app.js
+++ week1/9-13-homework/app.js
@@ -1,12 +1,19 @@
-1. // Verbal Questions
+/*1. // Verbal Questions
 a. A parameter is when you are defining a function. An argument is what you place when you are calling that function. 
 b. When you return a function you are producing the value however it may not print in the console. With console.log you are p rinting to the console the result of the function. 
 c. The function is called and required to be completed. It is implied that the arguments should be completed. 
-
+*/
 2. // Palindrome
 
-const checkPalindrome = () => {}
-
+const checkPalindrome = (word) => {
+	var splitString = word.split("")
+	var reverseArray = splitString.reverse();
+	var joinArray = reverseArray.join("")
+	console.log(word.split("").reverse().join("")); 
+	if (word === word.split("").reverse().join("")) {console.log(true)}
+		else {console.log(false)};
+}
+/*
 3. // Digit Sum
 
 const sumDigits = (num) => {
@@ -26,7 +33,7 @@ calculateSide(8, 6);
 
 5. // Sum Array
 
-6. // Prime Numbers
+6. a// Prime Numbers
 
 const checkPrime = (value) => {
     for(var i = 2; i < value; i++) {
@@ -37,3 +44,6 @@ const checkPrime = (value) => {
     return value > 1;
 }
 checkPrime(7)
+
+b
+*/
