1f2698511b5ef7e70786feaa22afaa3d11610d3c
Alex <climberfields@gmail.com>
2018-09-15 19:43:42 -0500

    done up to loops

---
+++ week1/9-14-homework/app.js
@@ -0,0 +1,46 @@
+let firstVariable = 'Hello World';
+firstVariable = firstVariable.toString();
+secondVariable = 
+
+
+
+
+
+
+BOOLEANS
+
+const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a _<_ b);
+  console.log(c _>_ d);
+  console.log('Name' _===_ 'Name');
+  console.log(true _!=_ false);
+  console.log(false _===_ false _===_ false _===_ false _===_ false _===_ false _!=_ true);
+  console.log(false _===_ false)
+  console.log(e _==__ 'Kevin');
+  console.log(a _<_ b _<__ c);
+  console.log(a _===_ a _<__ d);
+  console.log(48 _==_ '48');
+
+The Farm
+
+  var animal = 'panda';
+  if (animal === 'cow') {console.log('mooo')}
+  	else {console.log("Hey! You're not a cow!")}
+
+Driver's Ed
+
+let age = ;
+if (age >= 16) { console.log("Here are the keys.");} 
+else {console.log("Sorry you're too young.");}
+
+
+Loops
+
+a. 1 for ( let i = 0; i < 10; i++) {console.log(i)};
+   2 for (let i = 40; i < 400; i++) {console.log(i)};
+   
