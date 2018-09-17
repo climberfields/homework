a2f5b6bef2faa8639a678bc3ac715c80546494f9
Alex <climberfields@gmail.com>
2018-09-16 19:37:11 -0500

    did a few more

--- week1/9-14-homework/app.js
+++ week1/9-14-homework/app.js
@@ -41,9 +41,48 @@ else {console.log("Sorry you're too young.");}
 
 Loops
 
-a. 1 for ( let i = 0; i < 10; i++) {console.log(i)};
-   2 for (let i = 40; i < 400; i++) {console.log(i)};
+a. 1 for ( let i = 0; i <= 10; i++) {console.log(i)};
+   2 for (let i = 40; i <= 400; i++) {console.log(i)};
    3 for ( let i = 12; i <= 4000; i+3) {console.log(i)} 
    
-   b. for for (let i = 0; i < 100; i++) { if (i % 2 === 0) console.log(i);}
-   
+b. for (let i = 0; i <= 100; i++) { if (i % 2 === 0) console.log(i + " <-- this is an even number" );}
+
+
+ Give me Five 
+
+1. for (let i = 0; i < 100; i++) { if (i % 5 === 0) console.log(i + " High Five!");}
+
+2. for (let i = 0; i < 100; i++) { if (i % 3 === 0) console.log(i + " Three is a crowd");} 
+else if { (i % 5 === 0) {console.log(" High Five!");} else {console.log (i);}
+
+Savings account 
+
+1. var numbers = 
+
+Multiples of 3 and 5
+
+Arrays & Control Flow
+
+A.
+1. Objects
+2. They are in the order they are placed
+3. A grocery list
+
+Easy Does it
+
+B
+1. const my_sayings = ["Tomato Potato", "That's why turtles live so long", 
+"That's why turtles live so long"];
+
+C. Accessing Elements
+
+const randomThings = [1, 10, "Hello", true];
+1. randomThings[1]
+2. randomThings[2] = "World";
+
+D. Changing Values
+
+1. ourClass[2];
+ourClass[4] = "Octocat"
+ourClass.push("Cloud City");
+
