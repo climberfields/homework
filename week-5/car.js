39745673d942bc47707487a4fc07a983a565bcef
Alex <climberfields@gmail.com>
2018-10-07 20:09:22 -0500

    established the car, trying to export

---
+++ week-5/car.js
@@ -0,0 +1,16 @@
+class car {
+    constructor(color, convertible, speed) {
+    this.color = blue;
+    this.convertible = false;
+    this.speed = 0;
+}
+   get accelerate(speed) {
+       return this.speed + "10";
+   } 
+    get decelerate(speed) {
+        return this.speed - "10";
+    }
+}
+
+module.exports.car;
+
